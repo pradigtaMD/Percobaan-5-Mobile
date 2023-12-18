d741576e8728563b641847a1942c2e6295b7894c
Pradigta <120742640+pradigtaMD@users.noreply.github.com>
2023-12-18 23:40:39 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Percobaan 5 (F55122072"
+include(":app")
+ 
